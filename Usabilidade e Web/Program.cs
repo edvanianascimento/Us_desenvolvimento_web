e0ebc800c035d5f7a54322ce47d421fe3817c6b7
Edvania Evangelista <edvanianascimento@yahoo.com.br>
2022-03-15 20:53:26 -0300

    first commit

---
+++ Usabilidade e Web/Program.cs
@@ -0,0 +1,14 @@
+﻿< html >
+< head >
+< title > Exemplo CSS </ title >
+   </ head >
+   < body >
+   < h1 style = "font-style: italic;" >
+    A Voz do Morro
+    </ h1 >
+    < p style = "color: red;" >
+    Eu sou o samba <br>
+Sou natural daqui do Rio de Janeiro
+</p>
+</body>
+    </html>
